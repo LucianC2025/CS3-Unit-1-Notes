9c4782f13f9d329c16b04165932ec51acb50377a
LucianC2025 <lucian.cruz2025@gbwl.org>
2024-09-10 19:09:44 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
+def main(): 
     print("hello world")
 
+    # Declare variables
+    age = 17
+    teacher = "Ms. Navab"
+    is_fav_class = True 
+    my_grade = 99.99 
+    
 
 if __name__ == "__main__":
     main()
